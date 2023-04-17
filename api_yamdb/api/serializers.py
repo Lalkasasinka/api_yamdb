8fc1b29004f6a58ff4191b2be8e7021d5d66d46e
GLXY Opal <glxyopal@gmail.com>
2023-04-17 23:15:58 +0300

    user serializer and some view add

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,35 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-from reviews.models import Category, Title, Genre, Comment, Review
+from rest_framework.validators import UniqueValidator
+from reviews.models import (Category, Comment, Genre, Review, Title,
+                            User, NAME_LIMIT, EMAIL_LIMIT)
+from .validators import validate_username
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=[validate_username,
+                    UniqueValidator(queryset=User.objects.all())
+                    ],
+                    max_lenght=NAME_LIMIT
+    )
+
+    class Meta:
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'role')
+        model = User
+
+
+class TokenSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        max_lenght=NAME_LIMIT, validators=[validate_username],
+        )
+    confirmation_code = serializers.CharField()
+
+
+class SignUpSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=EMAIL_LIMIT,)
+    username = serializers.CharField(
+        validators=[validate_username], max_length=NAME_LIMIT)
 
 
 class CommentSerializer(serializers.ModelSerializer):
