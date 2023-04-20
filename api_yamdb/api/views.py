9cf0fb8de0bea2e952fa5b9ca9c07edd2e596ab7
GLXY Opal <glxyopal@gmail.com>
2023-04-20 20:57:51 +0300

    user viewset and urls add

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, filters, mixins, permissions, viewsets
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from rest_framework import viewsets, filters, mixins, permissions, viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action, api_view
+from rest_framework_simplejwt.tokens import AccessToken
 
 from reviews.models import Category, Genre, Title, Review, User
 from .serializers import (TitleSerializer, CategorySerializer,
@@ -9,6 +13,64 @@ from .serializers import (TitleSerializer, CategorySerializer,
                           SignUpSerializer)
 from .permissions import (IsAdmin, IsAdminModeratorOwnerOrReadOnly,
                           IsAdminOrReadOnly)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdmin,)
+    lookup_field = 'username'
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('username', )
+    http_method_names = ('patch', 'post', 'get', 'delete')
+
+    @action(methods=['get', 'patch'], detail=False,
+            permission_classes=(permissions.IsAuthenticated,), url_path='me')
+    def get_current_user_info(self, request):
+        serializer = UserSerializer(request.user)
+        if request.method == 'PATCH':
+            user = request.user
+            serializer = self.get_serializer(
+                user, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(role=request.user.role)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
+
+
+@api_view(['POST'])
+def api_get_token(request):
+    serializer = TokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    data = serializer.validated_data
+    username = serializer.validated_data.get('username')
+    user = get_object_or_404(User, username=username)
+    if default_token_generator.check_token(user, data['confirmation_code']):
+        return Response(
+            {'token': str(AccessToken.for_user(user))},
+            status=status.HTTP_201_CREATED)
+    return Response(serializer.validated_data,
+                    status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def api_signup(request):
+    serializer = SignUpSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.validated_data['username']
+    email = serializer.validated_data['email']
+    try:
+        user, _ = User.objects.get_or_create(
+            **serializer.validated_data
+        )
+    except Exception:
+        return Response(
+            'Такой username или e-mail уже используется.',
+            status=status.HTTP_400_BAD_REQUEST)
+    code = default_token_generator.make_token(user)
+    message = f'Здравствуйте, {username}! Ваш код подтверждения: {code}'
+    send_mail(_, message, 'support@yamdb.com', [email])
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -47,11 +109,3 @@ class TitleViewSet(viewsets.ModelViewSet):
 
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (IsAdmin,)
-    filter_backends = (filters.SearchFilter)
-    search_fields = ('username',)
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'api',
